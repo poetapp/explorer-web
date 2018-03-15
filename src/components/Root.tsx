2670c19ae7cbe35dedf03d60d139adb877f86696
Lautaro Dragan <lautaro.dragan@gmail.com>
2018-03-15 19:15:06 -0300

    Remove search, alpha-warning and some left-over login stuff (#19)

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,11 +1,10 @@
-import * as React from 'react';
-import { connect } from 'react-redux';
-import { Tabs } from 'react-tabs';
-import * as classNames from 'classnames'
+import * as React from 'react'
+import { connect } from 'react-redux'
+import { Tabs } from 'react-tabs'
 
-import '../extensions/String';
+import '../extensions/String'
 
-import { Navbar } from "./organisms/Navbar"
+import { Navbar } from './organisms/Navbar'
 import { Footer } from './organisms/Footer'
 
 import 'react-datepicker/dist/react-datepicker.css'
@@ -15,7 +14,6 @@ import './Root.scss'
 Tabs.setUseDefaultStyles(false);
 
 interface RootLayoutProps {
-  readonly loggedIn?: boolean;
   readonly location?: {
     readonly pathname: string;
   }
@@ -23,44 +21,34 @@ interface RootLayoutProps {
 }
 
 function render(props: RootLayoutProps) {
-  // TODO: a way for pages to provide these so the RootLayout doesn't need to know what urls are mapped to what pages
-  const location = props.location.pathname.trimLeft('/');
+  // TODO: templates rather than this url-matching monstrosity
+  const location = props.location.pathname.trimLeft('/')
 
-  const worksUrl = 'works';
-  const loginUrl = 'login';
-  const marketingLandingUrl = 'marketing-landing';
-  const isLoggedIn = props.loggedIn || !location;
+  const worksUrl = 'works'
 
-  const navbarShadow = ![worksUrl, ''].includes(location);
-  const navbarTransparent = ([''].includes(location)) && isLoggedIn;
-  const navbarMargin = ![worksUrl].includes(location);
-  const displayNavbarLogo = ![''].includes(location) || !isLoggedIn;
-  const displayNavbarSearch = ![''].includes(location) || !isLoggedIn;
-  const searchShadow = [worksUrl].includes(location);
-  const displayNavbar = ![loginUrl, marketingLandingUrl].includes(location);
-  const displayAlpha = location !== '' && location !== 'login';
+  const navbarShadow = ![worksUrl, ''].includes(location)
+  const navbarTransparent = ([''].includes(location))
+  const displayNavbarLogo = ![''].includes(location)
+  const displayNavbarSearch = false && ![''].includes(location)
+  const searchShadow = [worksUrl].includes(location)
 
   return (
     <div className="root-layout">
-      { displayNavbar && <Navbar
+      <Navbar
         shadow={navbarShadow}
         displayLogo={displayNavbarLogo}
         displaySearch={displayNavbarSearch}
         transparent={navbarTransparent}
         searchShadow={searchShadow}
-      /> }
-      { displayAlpha && <section className={classNames('alpha', navbarMargin && 'margin')}>
-      </section> }
+      />
       { props.children }
-      { location !== loginUrl && <Footer/> }
+      <Footer/>
     </div>
-  );
+  )
 }
 
 function mapStateToProps(state: any): RootLayoutProps {
-  return {
-
-  }
+  return {}
 }
 
 export const Layout = connect(mapStateToProps)(render);
