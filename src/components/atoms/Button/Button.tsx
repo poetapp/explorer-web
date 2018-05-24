d729efa3c44a145bad13d4563dbf4daef2e45a9b
jwicks31 <jessewicks@gmail.com>
2018-05-24 12:46:07 -0500

    updated button storybook & fixed import paths

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,6 +1,21 @@
 import * as React from 'react'
+import * as classNames from 'classnames'
 import './Button.scss'
 
-export const Button = () => {
-  return <button className="Button"> Testing </button>
+
+interface ButtonProps {
+  readonly className?: string
+  readonly text?: string
+  readonly disabled?: boolean
+  readonly onClick?: any
+  readonly type?: 'primary' | 'danger'
+}
+
+
+export const Button = (props: ButtonProps) => {
+  return <button
+    className={classNames('Button', props.className, `Button__${props.type}`)}
+    disabled={props.disabled}
+    onClick={props.onClick}
+    > {props.text} </button>
 }
