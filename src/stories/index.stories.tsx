a78c2972c3de0e297954e722a90ede1dbf1152ee
jwicks31 <jessewicks@gmail.com>
2018-05-30 16:32:47 -0500

    linting fix

--- src/stories/index.stories.tsx
+++ src/stories/index.stories.tsx
@@ -1,32 +1,27 @@
-import * as React from 'react'
-
 import { withInfo } from '@storybook/addon-info'
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import { linkTo } from '@storybook/addon-links'
 import { text, boolean } from '@storybook/addon-knobs/react'
 
-  const wInfoStyle = {
-    header: {
-      h1: {
-        marginRight: '20px',
-        fontSize: '25px',
-        display: 'inline',
-      },
-      body: {
-        paddingTop: 0,
-        paddingBottom: 0,
-      },
-      h2: {
-        display: 'inline',
-        color: '#999',
-      }
+const wInfoStyle = {
+  header: {
+    h1: {
+      marginRight: '20px',
+      fontSize: '25px',
+      display: 'inline',
     },
-    infoBody: {
-      backgroundColor: '#eee',
-      padding: '0px 5px',
-      lineHeight: '2',
-    }
-  }
-  
-export const wInfo = (text: string) => withInfo({ inline: true, source: false, styles: wInfoStyle, text: text });
+    body: {
+      paddingTop: 0,
+      paddingBottom: 0,
+    },
+    h2: {
+      display: 'inline',
+      color: '#999',
+    },
+  },
+  infoBody: {
+    backgroundColor: '#eee',
+    padding: '0px 5px',
+    lineHeight: '2',
+  },
+}
+
+export const wInfo = (text: string) => withInfo({ inline: true, source: false, styles: wInfoStyle, text })
