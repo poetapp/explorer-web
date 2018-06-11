63832449c9fe26dae7cf2d2fef09bddde07a1a4e
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-06-11 15:53:18 -0300

    Security Headers (#63)

--- webpack.config.js
+++ webpack.config.js
@@ -66,6 +66,11 @@ function getPlugins(environment) {
         to: './_redirects',
         toType: "file",
       },
+      {
+        from: './_headers',
+        to: './_headers',
+        toType: "file",
+      }
     ])
   ]
 
