e81723fe5454b978f94f3baad4785b0cbf296a3b
Jesse Wicks <jessewicks@gmail.com>
2018-11-06 14:41:09 -0600

    text changed back to content

--- src/pages/Work/Overview.tsx
+++ src/pages/Work/Overview.tsx
@@ -31,7 +31,7 @@ export class Overview extends WorkById {
           dateModified: Date.now().toString(),
           author: '',
           lastModified: '',
-          text: '',
+          content: '',
           tags: '',
         },
       },
