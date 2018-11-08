fe0914684ae7b4452487e22176c9ca495ccb3d9a
Jesse Wicks <jessewicks@gmail.com>
2018-11-08 17:31:24 -0600

    added env variables for frostApiUrl/apiUrl

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,10 +1,12 @@
+import { map, pipe, fromPairs, keys } from 'ramda'
+
 interface Configuration {
+  readonly frostApiUrl: string
   readonly apiUrl: string
   readonly dateFormat: string
   readonly dateTimeFormat: string
   readonly pagination: Pagination
-  readonly useMockSigner: boolean
-  readonly frostApiUrl: string
+  readonly useMockSigner?: boolean
 }
 
 interface Pagination {
@@ -12,4 +14,58 @@ interface Pagination {
   readonly visiblePageCount: number
 }
 
-export const Configuration: Configuration = require('Configuration')
+export const defaultConfiguration: Configuration = {
+  frostApiUrl: 'http://localhost:3000',
+  apiUrl: "http://localhost:18080",
+  dateFormat: "MMMM Do YYYY",
+  dateTimeFormat: "MMMM Do YYYY, HH:mm:ss",
+  pagination: {
+    limit: 10,
+    visiblePageCount: 6
+  },
+}
+
+export const camelCaseToScreamingSnakeCase = (camelCase: string = '') =>
+  camelCase.replace(/([A-Z])/g, capital => '_' + capital).toUpperCase()
+
+const toPair = (s: string) => [camelCaseToScreamingSnakeCase(s), s]
+
+const extractValue = (value: any) => {
+  const coercedValue = value === 'true' ? true : value === 'false' ? false : value
+
+  return isNaN(coercedValue)
+    ? coercedValue
+    : typeof coercedValue === 'boolean'
+      ? coercedValue
+      : parseInt(coercedValue, 10)
+}
+
+export const createEnvToConfigurationKeyMap: (keys: ReadonlyArray<string>) => {
+  readonly [index: string]: string,
+} = pipe(
+  map(toPair),
+  fromPairs,
+)
+
+const loadConfigurationFromEnv = (env: any): Partial<Configuration> => {
+  const map = createEnvToConfigurationKeyMap(keys(defaultConfiguration))
+
+  const configurationFromEnv = Object.entries(env)
+    .filter(([key, value]) => map[key])
+    .filter(([key, value]) => value !== undefined)
+    .reduce(
+      (previousValue, [key, value]: [string, any]) => ({
+        ...previousValue,
+        [map[key]]: extractValue(value),
+      }),
+      {},
+    )
+  return configurationFromEnv
+}
+
+export const getConfigurationWithDefaults = (envVars: any = {}) => ({
+  ...defaultConfiguration,
+  ...loadConfigurationFromEnv(envVars),
+})
+
+export const Configuration = getConfigurationWithDefaults(process.env)
