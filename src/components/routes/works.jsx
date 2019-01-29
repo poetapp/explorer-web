76168068a1e28bf06318e93b9757bf6eb3585d49
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-01-29 16:56:26 -0300

    remove incorrect copy pasta

--- src/components/routes/works.jsx
+++ src/components/routes/works.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 import { Works as WorksPage } from 'components/pages/works'
 
 export const Works = ({ match }) => {
-  const { id } = match.params
   return (
-    <WorksPage id={id} />
+    <WorksPage/>
   )
 }
