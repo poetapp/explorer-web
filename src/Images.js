5362b4c9e13c82322de5aa5354d8427b7c775985
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-02-03 04:11:48 -0300

    add images

---
+++ src/Images.js
@@ -0,0 +1,8 @@
+export Logo from 'images/poet-logo.svg'
+export LogoWhite from 'images/poet-logo-white.svg'
+export Quill from 'images/poet-quill.svg'
+
+export DefaultAvatar from 'images/anonymous-avatar-sm.jpg'
+
+export IPFS from 'images/ipfs.png'
+export Bitcoin from 'images/bitcoin.png'
