3d3b7f503435e489cb65388e2caa6ef9c6d6b7af
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-02-07 08:56:57 -0300

    sign out

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -1,28 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 
 import { Quill } from 'Images'
+import { SessionContext } from 'providers/SessionProvider'
 
 import { main, arrow } from './Main.scss'
 
-export const Main = ({ children }) => (
-  <section className={main}>
-    <header>
-      <Link to="/">
-        <img src={Quill} />
-      </Link>
-      <ul>
-        <li><Link to="/works">Explore</Link></li>
-        <li><a href="https://docs.poetnetwork.net/" target="_blank">Docs</a></li>
-        <li><a href="https://www.po.et/integrate" target="_blank">Integrate</a></li>
-        <li className={arrow}>More</li>
-      </ul>
-      <ul>
-        <li><Link to="/login">Login</Link></li>
-      </ul>
-    </header>
-    <main>
-      { children }
-    </main>
-  </section>
+const SessionActions = ({ token, onSignOut }) => !token ? (
+  <ul>
+    <li><Link to="/login">Login</Link></li>
+  </ul>
+) : (
+  <ul>
+    <li><a href="#" onClick={onSignOut}>Sign Out</a></li>
+  </ul>
 )
+
+export const Main = ({ children }) => {
+  const [token, setToken] = useContext(SessionContext)
+
+  const clearToken = () => setToken()
+
+  return (
+    <section className={main}>
+      <header>
+        <Link to="/">
+          <img src={Quill} />
+        </Link>
+        <ul>
+          <li><Link to="/works">Explore</Link></li>
+          <li><a href="https://docs.poetnetwork.net/" target="_blank">Docs</a></li>
+          <li><a href="https://www.po.et/integrate" target="_blank">Integrate</a></li>
+          <li className={arrow}>More</li>
+        </ul>
+        <SessionActions token={token} onSignOut={clearToken} />
+      </header>
+      <main>
+        { children }
+      </main>
+    </section>
+  )
+}
