17c9534499e2ed5a68d653043062157a86b5840f
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-02-07 09:05:19 -0300

    remove unused param

--- src/components/routes/Login.jsx
+++ src/components/routes/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Login as LoginPage } from 'components/pages/Login'
 
-export const Login = ({ match }) => {
+export const Login = () => {
   return (
     <LoginPage/>
   )
