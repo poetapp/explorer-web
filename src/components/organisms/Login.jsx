81def5a47f3552076b93a6183b8f4ea041b2689e
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-02-07 09:21:55 -0300

    use a form

--- src/components/organisms/Login.jsx
+++ src/components/organisms/Login.jsx
@@ -12,26 +12,33 @@ export const Login = ({ onSubmit }) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const onSubmitWrapper = credentials => event => {
+    event.preventDefault()
+    onSubmit(credentials)
+  }
+
   return (
     <section className={classNames.login}>
       <Link to='/'><img src={Logo} /></Link>
       <h1>Log Into My Account</h1>
       <h2>Log in to make, view, and manage your claims on the Po.et Network.</h2>
-      <input
-        type="text"
-        placeholder="Email"
-        value={email}
-        onChange={pipe(eventToValue, setEmail)}
-        required
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={pipe(eventToValue, setPassword)}
-        required
-      />
-      <button type="submit" onClick={() => onSubmit({ email, password })}>Log In</button>
+      <form>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={pipe(eventToValue, setEmail)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={pipe(eventToValue, setPassword)}
+          required
+        />
+        <button type="submit" onClick={onSubmitWrapper({ email, password })}>Log In</button>
+      </form>
     </section>
   )
 }
