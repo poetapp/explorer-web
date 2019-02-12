6034c9588ca6261cc7a278b5df04e11ba30ec758
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-02-12 02:30:32 -0300

    fancier

--- src/components/pages/ConfirmMail.jsx
+++ src/components/pages/ConfirmMail.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import { useConfirmEmail } from 'effects/useConfirmMail'
 
 export const ConfirmMail = ({ token }) => {
   const { loginToken, error } = useConfirmEmail(token)
 
-  return (
-    <section>
-      <div>Success:{loginToken}</div>
-      <div>Error:{error}</div>
-    </section>
-  )
+  return !loginToken && !error
+    ? <InProgress />
+    : loginToken
+    ? <Success />
+    : <Failure reason={error} />
 }
+
+const InProgress = () => (
+  <section><h1>Verifying your email...</h1></section>
+)
+
+const Success = () => (
+  <section>
+    <h1>Thanks for verifying your email :)</h1>
+    <h2>Click <Link to="/">here</Link> to go to your dashboard.</h2>
+  </section>
+)
+
+const Failure = ({ reason }) => (
+  <section>
+    <h1>Oww, we could not verify your email address :(</h1>
+    <h2>Looks like {reason}</h2>
+  </section>
+)
