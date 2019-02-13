a7925b7760c968e18284a842c02c06ae0f9fba06
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-02-13 02:04:47 -0300

    useProfile wip

--- src/components/pages/ConfirmMail.jsx
+++ src/components/pages/ConfirmMail.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 import { useConfirmEmail } from 'effects/useConfirmMail'
+import { useProfile } from 'effects/useProfile'
 import { ConfirmEmail as ConfirmEmailOrganism } from 'components/organisms/ConfirmEmail'
 
 export const ConfirmMail = ({ token }) => {
   const { loginToken, error } = useConfirmEmail(token)
+  const { profile, error: profileError } = useProfile(loginToken)
+
+  console.log('profile', profile)
+  console.log('profileError', profileError)
 
   return <ConfirmEmailOrganism loginToken={loginToken} error={error} />
 }
