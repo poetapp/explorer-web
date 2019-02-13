7adcc5b9b53ac0cc9b2489ff1257034979b6d322
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-02-13 04:02:06 -0300

    set account wip

--- src/components/pages/ConfirmMail.jsx
+++ src/components/pages/ConfirmMail.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
 
+import { ConfirmEmail as ConfirmEmailOrganism } from 'components/organisms/ConfirmEmail'
 import { useConfirmEmail } from 'effects/useConfirmMail'
 import { useProfile } from 'effects/useProfile'
-import { ConfirmEmail as ConfirmEmailOrganism } from 'components/organisms/ConfirmEmail'
+import { SessionContext } from 'providers/SessionProvider'
+
+const useIfTruthy = fn => value => useEffect(() => {
+  if (value)
+    fn(value)
+}, [value])
+
+const useLogIfTruthy = useIfTruthy(console.error.bind(console))
 
 export const ConfirmMail = ({ token }) => {
-  const { loginToken, error } = useConfirmEmail(token)
+  const { loginToken, error: confirmEmailError } = useConfirmEmail(token)
   const { profile, error: profileError } = useProfile(loginToken)
+  const [account, setAccount] = useContext(SessionContext)
 
+  console.log('account', account)
   console.log('profile', profile)
-  console.log('profileError', profileError)
 
-  return <ConfirmEmailOrganism loginToken={loginToken} error={error} />
+  useEffect(() => {
+    if (loginToken && profile)
+      setAccount({
+        token: loginToken,
+        email: profile.email,
+      })
+  }, [loginToken, profile])
+
+  useLogIfTruthy(confirmEmailError)
+  useLogIfTruthy(profileError)
+
+  return <ConfirmEmailOrganism loginToken={loginToken} error={confirmEmailError} />
 }
