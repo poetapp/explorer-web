ce8ff4a2e998f0281f67e552c4022fe3c735b3d6
lautarodragan <lautaro.dragan@gmail.com>
2019-02-14 02:17:58 -0300

    funcitonality

---
+++ src/components/organisms/Tokens.jsx
@@ -0,0 +1,51 @@
+import moment from 'moment'
+import React from 'react'
+
+import classNames from './Tokens.scss'
+
+export const Tokens = ({ tokens }) => {
+  return (
+    <section className={classNames.tokens}>
+      <h1>API Tokens</h1>
+      <h2>Manage your API Tokens by authenticating with the Frost API</h2>
+      <TokenTable tokens={tokens} />
+    </section>
+  )
+}
+
+const TokenTable = ({ tokens }) => (
+  <table>
+    <thead>
+      <tr>
+        <td>#</td>
+        <td>Token</td>
+        <td>Creation</td>
+        <td>Expiry</td>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        tokens?.map(token =>
+          <TokenTableRow token={token} key={token.serializedToken} />
+        )
+      }
+    </tbody>
+  </table>
+)
+
+const TokenTableRow = ({ token }) => {
+  const firstAndLastCharacters = (s, n) => s.slice(0, n) + '...' + s.slice(-n)
+  const formatDate = date => moment(date).format('DD/MM/YY')
+
+  const formattedIat = formatDate(token.iat)
+  const formattedSerializedToken = firstAndLastCharacters(token.serializedToken, 20)
+
+  return (
+    <tr>
+      <td>1</td>
+      <td>{formattedSerializedToken}</td>
+      <td>{formattedIat}</td>
+      <td>Never</td>
+    </tr>
+  )
+}
--- src/components/pages/Tokens.jsx
+++ src/components/pages/Tokens.jsx
@@ -1,23 +1,22 @@
 import React, { useContext } from 'react'
 
 import { Main } from 'components/templates/Main'
+import { Tokens as TokensOrganism } from 'components/organisms/Tokens'
+import { parseJwt } from 'helpers/jwt'
+import { useTokens } from 'hooks/useTokens'
 import { SessionContext } from 'providers/SessionProvider'
-import {useTokens} from '../../hooks/useTokens'
-
 
 export const Tokens = () => {
   const [account] = useContext(SessionContext)
   const [tokens, error] = useTokens(account?.token)
+  const parsedTokens = tokens?.map(serializedToken => ({
+    ...parseJwt(serializedToken),
+    serializedToken,
+  }))
 
   return (
     <Main>
-      <ul>
-        {
-          tokens?.map(token =>
-            <li key={token}>{token}</li>
-          )
-        }
-      </ul>
+      <TokensOrganism tokens={parsedTokens} />
     </Main>
   )
 }
