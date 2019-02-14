36d76cf313ca535a903e58455ff96b4d2e999853
lautarodragan <lautaro.dragan@gmail.com>
2019-02-14 02:18:26 -0300

    fix iat

--- src/components/organisms/Tokens.jsx
+++ src/components/organisms/Tokens.jsx
@@ -37,7 +37,7 @@ const TokenTableRow = ({ token }) => {
   const firstAndLastCharacters = (s, n) => s.slice(0, n) + '...' + s.slice(-n)
   const formatDate = date => moment(date).format('DD/MM/YY')
 
-  const formattedIat = formatDate(token.iat)
+  const formattedIat = formatDate(token.iat * 1000)
   const formattedSerializedToken = firstAndLastCharacters(token.serializedToken, 20)
 
   return (
