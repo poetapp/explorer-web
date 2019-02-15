e4e8b982b779d212a19d7e0cc7a87025be2509dd
lautarodragan <lautaro.dragan@gmail.com>
2019-02-15 04:38:57 -0300

    improve error handling in useTokens

--- src/components/pages/Tokens.jsx
+++ src/components/pages/Tokens.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 
 import { Main } from 'components/templates/Main'
 import { Tokens as TokensOrganism } from 'components/organisms/Tokens'
@@ -8,14 +8,18 @@ import { SessionContext } from 'providers/SessionProvider'
 
 export const Tokens = () => {
   const [account, setAccount] = useContext(SessionContext)
-  const [tokens, error] = useTokens(account?.token)
+  const [tokens, serverError, clientError] = useTokens(account?.token)
   const parsedTokens = tokens?.map(serializedToken => ({
     ...parseJwt(serializedToken),
     serializedToken,
   }))
 
-  if (error) // === 'Expired token.'
-    setAccount(null)
+  useEffect(() => {
+    if (serverError || clientError) {
+      console.error(serverError || clientError)
+      setAccount(null)
+    }
+  }, [serverError, clientError])
 
   return (
     <Main>
--- src/hooks/useTokens.js
+++ src/hooks/useTokens.js
@@ -9,11 +9,12 @@ const fetchTokens = token => fetch(`https://api.poetnetwork.net/tokens`, {
 export const useTokens = (parentToken) => {
   const [response, setResponse] = useState()
   const [tokens, setTokens] = useState()
-  const [error, setError] = useState()
+  const [serverError, setServerError] = useState()
+  const [clientError, setClientError] = useState()
 
   useEffect(() => {
     if (parentToken)
-      fetchTokens(parentToken).then(setResponse).catch(setError)
+      fetchTokens(parentToken).then(setResponse).catch(setClientError)
   }, [parentToken])
 
   useEffect(() => {
@@ -22,9 +23,9 @@ export const useTokens = (parentToken) => {
     if (response.status === 200) {
       response.json().then(_ => _.apiTokens).then(setTokens)
     } else {
-      setError(response.statusText)
+      setServerError(response.statusText)
     }
   }, [response])
 
-  return [tokens, error]
+  return [tokens, serverError, clientError]
 }
