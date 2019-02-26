c90ede4729e23a23f6c30e6122c401f43746a18c
lautarodragan <lautaro.dragan@gmail.com>
2019-02-26 19:30:34 -0300

    styles

--- src/components/pages/NewClaim.jsx
+++ src/components/pages/NewClaim.jsx
@@ -7,7 +7,21 @@ import classNames from './NewClaim.scss'
 export const NewClaim = () => {
   return (
     <Main>
-      <section className={classNames.newClaim}>...</section>
+      <section className={classNames.newClaim}>
+        <h1>New Claim</h1>
+        <h2>Create a New Claim on the Po.et Network</h2>
+        <form>
+          <label for="">Name</label>
+          <input type="text" id="name" />
+          <label for="">Author Name</label>
+          <input type="text" id="author" />
+          <label for="">Tags</label>
+          <input type="text" id="name" />
+          <label for="">Date Created</label>
+          <input type="text" id="date" />
+          <button type="submit">Submit</button>
+        </form>
+      </section>
     </Main>
   )
 }
