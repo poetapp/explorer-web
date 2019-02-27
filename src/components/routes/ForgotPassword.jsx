96ec8235362a19f9e5081b3359328e8d3660c70f
lautarodragan <lautaro.dragan@gmail.com>
2019-02-27 13:48:29 -0300

    delete paramless routes

--- src/components/routes/ForgotPassword.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { ForgotPassword as ForgotPasswordPage } from 'components/pages/ForgotPassword'
-
-export const ForgotPassword = () => {
-  return (
-    <ForgotPasswordPage/>
-  )
-}
--- src/components/routes/SignUp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { SignUp as SignUpPage } from 'components/pages/SignUp'
-
-export const SignUp = () => {
-  return (
-    <SignUpPage/>
-  )
-}
