6d53a6e8745ef86f6cc41e50489ca4772029fff2
lautarodragan <lautaro.dragan@gmail.com>
2019-03-09 19:37:55 -0300

    merge sign up page and organism

--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -1,18 +1,67 @@
-import React, { useState, useEffect, useContext } from 'react'
+import { pipe } from 'ramda'
+import React, { useState, useContext } from 'react'
+import { Link } from 'react-router-dom'
 
-import { SignUp as SignUpOrganism } from 'components/organisms/SignUp'
+import { Logo } from 'Images'
+
+import { eventToValue } from 'helpers/eventToValue'
 import { SessionContext } from 'providers/SessionProvider'
 import { ApiContext } from 'providers/ApiProvider'
 
+import classNames from './SignUp.scss'
+
 export const SignUp = () => {
   const [api, isBusy] = useContext(ApiContext)
-  const [credentials, setCredentials] = useState(null)
   const [_, setAccount] = useContext(SessionContext)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordRepeat, setPasswordRepeat] = useState('')
 
-  useEffect(() => {
-    if (credentials)
-      api.accountCreate(credentials).then(setAccount)
-  }, [credentials])
+  const onSubmit = event => {
+    event.preventDefault()
+    if (password === passwordRepeat)
+      api.accountCreate({ email, password }).then(setAccountFromApiResponse)
+  }
 
-  return <SignUpOrganism onSubmit={setCredentials} />
+  const setAccountFromApiResponse = token => token && setAccount({
+    token,
+    email,
+  })
+
+  return (
+    <section className={classNames.signup}>
+      <Link to='/'><img src={Logo} /></Link>
+      <h1>Sign Up to Start Using Po.et Today</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={pipe(eventToValue, setEmail)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={pipe(eventToValue, setPassword)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Repeat Password"
+          value={passwordRepeat}
+          onChange={pipe(eventToValue, setPasswordRepeat)}
+          required
+        />
+        <p>Signing up means that you have read and agreed to the <Link to="/tos">terms of service</Link></p>
+        <nav>
+          <button type="submit" onClick={onSubmit}>Sign Up</button>
+          <nav>
+            <Link to="/login">Already have an account?</Link>
+          </nav>
+        </nav>
+      </form>
+    </section>
+  )
 }
