f391845255733b2db5b1af2f2dae4aeae71465c0
lautarodragan <lautaro.dragan@gmail.com>
2019-03-19 23:49:22 -0300

    merge issuer organism and page

--- src/components/pages/Issuer.jsx
+++ src/components/pages/Issuer.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
 
+import { DefaultAvatar, LogoWhite } from 'Images'
+
+import { Works } from 'components/molecules/Works'
 import { Main } from 'components/templates/Main'
-import { IssuerById as IssuerByIdOrganism } from 'components/organisms/Issuer'
 import { useWorkByIssuer } from 'hooks/useWork'
+
+import classNames from './Issuer.scss'
 
 export const IssuerById = ({ id }) => {
   const works = useWorkByIssuer(id)
 
   return (
     <Main>
-      <IssuerByIdOrganism works={works} />
+      <section className={classNames.issuer}>
+        <div className={classNames.letterBoxing}>
+          <Profile name={works && works[0] && works[0].claim && works[0].claim.author || 'Author'} />
+          <Works works={works} />
+        </div>
+      </section>
     </Main>
   )
 }
+
+const Profile = ({ name }) => (
+  <section className={classNames.profile}>
+    <img src={DefaultAvatar}/>
+    <h1>{name}</h1>
+    <h2>Two roads diverged in a yellow wood, and sorry I could not travel both.</h2>
+    <span>contact@po.et</span>
+    <a href="#">
+      <img src={LogoWhite} />
+      <span>Tip POE</span>
+    </a>
+  </section>
+)
