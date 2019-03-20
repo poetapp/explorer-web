58c8f272a8e8f7d4e8bf4589f99262e091baabf9
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-03-19 21:05:04 -0300

    fix: date format in works by issuer (#608)

--- src/components/molecules/Works.jsx
+++ src/components/molecules/Works.jsx
@@ -1,3 +1,4 @@
+import moment from 'moment'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -15,6 +16,6 @@ export const Work = ({ work }) => (
   <li>
     <h1><Link to={`/works/${work.id}`}>{work.claim.name}</Link></h1>
     <h2>{work.claim.author}</h2>
-    <h3>{work.claim.datePublished}</h3>
+    <h3>{moment(work.claim.datePublished).format('MMMM Do, YYYY')}</h3>
   </li>
 )
