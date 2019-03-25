6c81f63b0b5e2fc16e85043d25afedc66d79dc77
lautarodragan <lautaro.dragan@gmail.com>
2019-03-25 14:20:41 -0300

    watch for possible setter changes in useEnvironmentNetworkConsole

--- src/hooks/useEnvironmentNetworkConsole.jsx
+++ src/hooks/useEnvironmentNetworkConsole.jsx
@@ -7,22 +7,26 @@ export const useEnvironmentNetworkConsole = () => {
   const [environment, setEnvironment] = useEnvironment()
   const [network, setNetwork] = useNetwork()
 
-  window.network = network
-  window.setNetwork = setNetwork
-  window.environment = environment
-  window.setEnvironment = setEnvironment
-  window.production = () => {
-    setEnvironment('production')
-    setNetwork('mainnet')
-  }
-  window.qa = () => {
-    setEnvironment('qa')
-    setNetwork('testnet')
-  }
+  useEffect(() => {
+    window.setEnvironment = setEnvironment
+    window.setNetwork = setNetwork
+
+    window.production = () => {
+      setEnvironment('production')
+      setNetwork('mainnet')
+    }
+    window.qa = () => {
+      setEnvironment('qa')
+      setNetwork('testnet')
+    }
+  }, [setEnvironment, setNetwork])
 
   useEffect(() => {
+    window.environment = environment
+    window.network = network
     console.log(`Using API environment '${environment}' and network '${network}'.`)
   }, [environment, network])
+
   useEffect(() => {
     console.log(`To change these use window.setEnvironment(environment), window.setNetwork(network), window.production() and window.qa().`)
   }, [])
