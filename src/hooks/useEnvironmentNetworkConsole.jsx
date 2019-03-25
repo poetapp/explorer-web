6e544684c290bb5448a48c773fc97000f5661fcb
lautarodragan <lautaro.dragan@gmail.com>
2019-03-25 00:49:49 -0300

    debugging information

--- src/hooks/useEnvironmentNetworkConsole.jsx
+++ src/hooks/useEnvironmentNetworkConsole.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+
 import { useEnvironment } from 'hooks/useEnvironment'
 import { useNetwork } from 'hooks/useNetwork'
 
@@ -9,5 +11,10 @@ export const useEnvironmentNetworkConsole = () => {
   window.setNetwork = setNetwork
   window.environment = environment
   window.setEnvironment = setEnvironment
+
+  useEffect(() => {
+    console.log(`Using API environment '${environment}' and network '${network}'.`)
+    console.log(`To change these use window.setEnvironment and window.setNetwork.`)
+  }, [environment, network])
 
 }
