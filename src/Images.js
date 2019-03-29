9970b3f6473b3f1b5b040ba65b62210251cad04b
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-03-28 22:28:31 -0300

    fix: use static URL for badge image (#638)

--- src/Images.js
+++ src/Images.js
@@ -1,6 +1,7 @@
 export Logo from 'images/poet-logo.svg'
 export LogoWhite from 'images/poet-logo-white.svg'
 export Quill from 'images/poet-quill.svg'
+export const QuillS3 = 'https://s3.amazonaws.com/poet-assets/poet-quill.svg'
 
 export DefaultAvatar from 'images/anonymous-avatar-sm.jpg'
 
