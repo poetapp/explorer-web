59fa6a4c95e12714c82ba6488898905b6fd511c7
lautarodragan <lautaro.dragan@gmail.com>
2019-07-02 22:08:09 -0300

    pagination in profile page

--- src/components/pages/Issuer.jsx
+++ src/components/pages/Issuer.jsx
@@ -1,23 +1,40 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 import { DefaultAvatar, LogoWhite } from 'Images'
 
 import { Works } from 'components/shared/Works'
+import { PaginationWrapper } from 'components/shared/Pagination'
 import { Main } from 'components/templates/Main'
 import { ApiContext } from 'providers/ApiProvider'
 
 import classNames from './Issuer.scss'
 
+const pageSize = 10
+
 export const IssuerById = ({ id }) => {
-  const [api, isBusy, useApi] = useContext(ApiContext)
-  const works = useApi('worksGetByFilters', { issuer: id })
+  const [api, isBusy] = useContext(ApiContext)
+  const [page, setPage] = useState(0)
+  const [works, setWorks] = useState([])
+  const [pageCount, setPageCount] = useState(0)
+
+  useEffect(() => {
+    const offset = page * 10
+    api && api.worksGetByFilters({ issuer: id, offset }).then(setWorks)
+  }, [api, page])
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+    setPageCount(works?.totalCount ? Math.floor(works.totalCount / pageSize) : 0)
+  }, [works])
 
   return (
     <Main>
       <section className={classNames.issuer}>
         <div className={classNames.letterBoxing}>
           <Profile issuer={id} />
-          <Works works={works || []} />
+          <PaginationWrapper pageCount={pageCount} value={page} onChange={setPage}>
+            <Works works={works || []} />
+          </PaginationWrapper>
         </div>
       </section>
     </Main>
