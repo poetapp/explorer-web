1c57ab240e01376a69db1e4b40cab128de467481
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-07-15 21:50:56 -0300

    docs: document usePersistedState's quirky behavior (#692)

--- src/hooks/usePersistedState.js
+++ src/hooks/usePersistedState.js
@@ -1,5 +1,38 @@
 import { useState, useEffect } from 'react'
 
+/**
+ * usePersistedState acts like React's useState, backing the value to and from LocalStorage.
+ *
+ * There's one caveat: every call to usePersistedState will allocate a new state entry in React, no matter the key.
+ * This means two different calls to usePersistedState with the same key will NOT benefit from React's
+ * re-renders when one updates.
+ *
+ * See the following example:
+ *
+ * ```
+ *  // initial state: 'initial value' stored in localStorage.key1
+ *
+ *  const [value, setValue] = usePersistentStorage('key1')
+ *  const [value2, setValue2] = usePersistentStorage('key1') // same key
+ *
+ *  assert(value === value2) // true
+ *
+ *  useEffect(() => {
+ *    setValue('new value')
+ *  }, [])
+ *
+ *  useEffect(() => {
+ *    assert(value === value2) // true the first time when conditions are initial, false after setValue is called since value2 isn't updated by React
+ *  }, [value])
+ *
+ *  useEffect(() => {
+ *    assert(value === value2) // true, but will not run when setValue is called
+ *  }, [value2])
+ * ```
+ *
+ * In practice this means this function should only be called once per key in the entire application.
+ * The best place to do so will usually be a Context Provider.
+ */
 export const usePersistedState = (key) => {
   const [state, setState] = useState(JSON.parse(window.localStorage.getItem(key)))
 
