52a46073fc762974525caea1fc1169698311a5e0
lautarodragan <lautaro.dragan@gmail.com>
2019-07-15 21:39:24 -0300

    .

--- src/hooks/usePersistedState.js
+++ src/hooks/usePersistedState.js
@@ -1,5 +1,13 @@
 import { useState, useEffect } from 'react'
 
+/**
+ * usePersistedState acts like React's useState, backing the value to and from LocalStorage.
+ * There's one caveat: every call to usePersistedState will allocate a new state entry, no matter the key.
+ * This means two different calls to usePersistedState with the same key will NOT benefit from React's
+ * re-renders when one updates.
+ * In practice this means this function should only be called once per key in the entire application.
+ * The best place to do so will usually be a Context Provider.
+ */
 export const usePersistedState = (key) => {
   const [state, setState] = useState(JSON.parse(window.localStorage.getItem(key)))
 
