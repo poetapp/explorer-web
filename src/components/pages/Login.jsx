f0f0b2b93b064b3d3ecad7b68feb04277ac4ea0c
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-07-15 22:51:29 -0300

    fix: login (#693)

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 import { Logo } from 'Images'
@@ -11,25 +11,37 @@ import { SessionContext } from 'providers/SessionProvider'
 import classNames from './Login.scss'
 
 export const Login = () => {
-  const [api, isBusy, useApi, environment] = useContext(ApiContext)
-  const [_, setAccount] = useContext(SessionContext)
+  const [api, isBusy, useApi, environment, network] = useContext(ApiContext)
+  const [account, setAccount] = useContext(SessionContext)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [loginResponse, setLoginResponse] = useState()
+  const [getAccountResponse, setGetAccountResponse] = useState()
 
-  const setAccountWithLoginData = ({ token, issuer }) => account => setAccount(account && {
-    ...account,
-    token,
-    email,
-    issuer,
-    environment,
-  })
-
-  const onSubmit = async event => {
+  const onSubmit = event => {
     event.preventDefault()
-    const loginResponse = await api.login({ email, password })
-    if (loginResponse)
-      api.accountGet(loginResponse.issuer).then(setAccountWithLoginData(loginResponse))
+    api.login({ email, password }).then(setLoginResponse)
   }
+
+  useEffect(() => {
+    if (loginResponse)
+      setAccount({ token: loginResponse.token })
+  }, [loginResponse])
+
+  useEffect(() => {
+    if (api?.token)
+      api.accountGet(loginResponse.issuer).then(setGetAccountResponse)
+  }, [api])
+
+  useEffect(() => {
+    if (getAccountResponse)
+      setAccount({
+        email,
+        environment,
+        ...loginResponse,
+        ...getAccountResponse,
+      })
+  }, [getAccountResponse])
 
   return (
     <section className={classNames.login}>
