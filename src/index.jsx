a754d14ff865ab2945ec5c50f1bee6c5dacf0600
lautarodragan <lautaro.dragan@gmail.com>
2019-07-18 21:12:42 -0300

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { render} from 'react-dom'
 import 'react-toastify/dist/ReactToastify.css'
 
-import { ApiClient } from 'src/helpers/ApiClient'
+import { ApiClient } from 'helpers/ApiClient'
 import { FrostApi } from 'apis/frost'
 
 import './index.scss'
