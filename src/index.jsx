f907ea22102d340e6ecae9971ba31c55b1f50348
lautarodragan <lautaro.dragan@gmail.com>
2019-07-22 13:57:32 -0300

    clean up index

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,6 @@ import * as React from 'react'
 import { render} from 'react-dom'
 import 'react-toastify/dist/ReactToastify.css'
 
-import { ApiClient } from 'helpers/ApiClient'
-import { FrostApi } from 'apis/frost'
-
 import './index.scss'
 
 import { App } from 'components/App'
@@ -12,7 +9,3 @@ import { App } from 'components/App'
 const reactRoot = document.getElementById('react-root');
 
 render(<App/>, reactRoot)
-
-const apiClient = ApiClient(FrostApi('https://api.poetnetwork.net', ''))
-
-window.apiClient = apiClient
