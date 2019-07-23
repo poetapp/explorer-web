fcaa451b0a6b2d0c52fe4f335dbd486cbca8279a
lautarodragan <lautaro.dragan@gmail.com>
2019-07-22 22:19:17 -0300

    remove flattenArray

--- src/helpers/array.js
+++ src/helpers/array.js
@@ -5,11 +5,3 @@ export const withTotalCount = (array, totalCount) => {
 }
 
 export const ofNumbers = (length, base = 0) => Array(length).fill(undefined).map((e, i) => i + base)
-
-export const flattenArray = (array) =>
-  array.reduce((acc, val) =>
-      Array.isArray(val)
-        ? [...acc, ...flattenArray(val)]
-        : [...acc, val],
-    [],
-  )
