2ce4c4005eeea6e1584c3c886e998c62801bf5c1
lautarodragan <lautaro.dragan@gmail.com>
2019-08-22 19:05:17 -0300

    todo

--- src/components/shared/ClaimGraph.jsx
+++ src/components/shared/ClaimGraph.jsx
@@ -116,7 +116,7 @@ const Figcaption = ({ currentClaim }) => {
 const Figure = ({ edges, currentClaim, setWorkId }) => {
   const figure = useRef(null)
   const g = useRef(null)
-  const graph = dagreFromEdges(edges)
+  const graph = dagreFromEdges(edges) // TODO: this needn't run on every render!
   const [dim, setDim] = useState(false)
   const [selectedNode, setSelectedNode] = useState(false)
   const [inner, setInner] = useState(false)
