4c5fcdb1488f56b323b5b51ee8e262ca669a53da
lautarodragan <lautaro.dragan@gmail.com>
2019-08-26 02:30:04 -0300

    fix?

--- src/components/shared/ClaimGraph.jsx
+++ src/components/shared/ClaimGraph.jsx
@@ -97,8 +97,8 @@ const renderGraph = ({ graph, onNodeSelected }) => {
 }
 
 const scaleGraph = (graph, svg, size) => {
-  const graphWidth = graph.graph().width + graphMargin
-  const graphHeight = graph.graph().height + graphMargin
+  const graphWidth = graph.graph().width
+  const graphHeight = graph.graph().height
   const zoomScale = Math.min(size.width / graphWidth, size.height / graphHeight)
   const translateX = (size.width / 2) - ((graphWidth * zoomScale) / 2)
   const translateY = (size.height / 2) - ((graphHeight * zoomScale) / 2)
